c8d4be26e0c031c0fdc24dbb6e9e1f7e47e3618a
Sazonov Georgy <gera950@inbox.ru>
2023-09-11 13:53:02 +0300

    Init commit

---
+++ ONXXTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SixLabors.ImageSharp; // Из одноимённого пакета NuGet
+using SixLabors.ImageSharp.PixelFormats;
+using System.Linq;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using Microsoft.ML.OnnxRuntime;
+using System.Collections.Generic;
+
+using MyYOLOApi;
+
+namespace YOLO_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string modelPath = "https://storage.yandexcloud.net/dotnet4/tinyyolov2-8.onnx";
+            string? imagePath;
+            Image<Rgb24> img;
+            ModelManager modelManager = new ModelManager(modelPath);
+
+            while (true)
+            {
+                imagePath = Console.ReadLine();
+                if (imagePath is null || imagePath == "")
+                    break;
+                img = Image.Load<Rgb24>(imagePath);
+                var results = modelManager.Predict(img);
+            }
+        }
+    }
+}
