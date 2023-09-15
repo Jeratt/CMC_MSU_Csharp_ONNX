c24572546671e916b728eb31bef7715b3b56f10f
Sazonov Georgy <gera950@inbox.ru>
2023-09-15 20:11:20 +0300

    +

--- ONXXTest/Program.cs
+++ ONXXTest/Program.cs
@@ -14,11 +14,14 @@ namespace YOLO_csharp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             string modelPath = "https://storage.yandexcloud.net/dotnet4/tinyyolov2-8.onnx";
             string? imagePath;
             Image<Rgb24> img;
+            List<Pair> res = new List<Pair>();
+            List<ObjectBox> lob;
+            List<Tuple<Task<List<ObjectBox>>, string>> tasks = new List<Tuple<Task<List<ObjectBox>>, string>>();
             ModelManager modelManager = new ModelManager(modelPath);
 
             while (true)
@@ -27,8 +30,83 @@ namespace YOLO_csharp
                 if (imagePath is null || imagePath == "")
                     break;
                 img = Image.Load<Rgb24>(imagePath);
-                var results = modelManager.Predict(img);
+                //lob = await modelManager.PredictAsync(img);
+                try
+                {
+                    tasks.Add(new Tuple<Task<List<ObjectBox>>, string>(modelManager.PredictAsync(img), imagePath));
+                }
+                catch(Exception x)
+                {
+                    Console.WriteLine(x.Message);
+                }
+/*                foreach (ObjectBox obj in lob)
+                {
+                    res.Add(new Pair(imagePath, obj));
+                }*/
             }
+            foreach(var t in tasks)
+            {
+                try
+                {
+                    imagePath = t.Item2;
+                    lob = await t.Item1;
+                    foreach (ObjectBox obj in lob)
+                    {
+                        res.Add(new Pair(imagePath, obj));
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            ObjectBoxToCSV(res, "results.csv");
+            if (File.Exists("results.csv"))
+            {
+                Console.WriteLine(Path.GetFullPath("results.csv"));
+
+                FileStream fs = File.OpenRead("results.csv");
+                StreamReader reader = new StreamReader(fs);
+                Console.WriteLine(reader.ReadToEnd());
+            }
+        }
+
+        static void ObjectBoxToCSV(List<Pair> pairs, string filename)
+        {
+            FileStream fs = null;
+            StreamWriter writer = null;
+            try
+            {
+                fs = File.Create(filename);
+                writer = new StreamWriter(fs);
+                writer.WriteLine("filename,class,x,y,w,h");
+                foreach (var p in pairs)
+                {
+                    writer.WriteLine($"{p.Filename},{p.Obj.Class},{p.Obj.XMin},{p.Obj.YMax},{p.Obj.XMax - p.Obj.XMin},{p.Obj.YMax - p.Obj.YMin}");
+                }
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine($"ERROR SAVING FILE ! ! !: {x}");
+                //throw;
+                return;
+            }
+            finally
+            {
+                writer?.Dispose();
+                fs?.Close();
+            }
+        }
+    }
+
+    class Pair
+    {
+        public ObjectBox Obj { get; set; }
+        public string Filename { get; set; }
+        public Pair(string filename, ObjectBox ob)
+        {
+            Filename = filename;
+            Obj = ob;
         }
     }
 }
